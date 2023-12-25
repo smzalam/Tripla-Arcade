1fdbe17d7be9374cf7b0bc95863eb2b1c0d3a47e
smzalam <alam.syedmz@gmail.com>
2023-12-24 21:57:21 -0500

    made placeholder for other games logic

--- server/src/socketHandlers/gameEvents.js
+++ server/src/socketHandlers/gameEvents.js
@@ -7,6 +7,8 @@ const gameMove = async (server, room, data) => {
     const gameState = await getGameState(room);
     if (data.game === 'TicTacToe') {
         TicTacToeGameMoveLogic(server, gameState, data);
+    } else {
+        TicTacToeGameMoveLogic(server, gameState, data);
     }
 }
 
@@ -14,6 +16,8 @@ const gameMove = async (server, room, data) => {
 const gameReset = async (server, room, data) => {
     const gameState = await getGameState(room);
     if (data.game === 'TicTacToe') {
+        TicTacToeGameResetLogic(server, gameState);
+    } else {
         TicTacToeGameResetLogic(server, gameState);
     }
 }
