f27570cb1860bb0fe42c11f782820cc70d6402f3
smzalam <alam.syedmz@gmail.com>
2023-12-31 19:12:41 -0500

    added auth route

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,8 @@ import { createServer } from 'node:http';
 import createSocket from "./sockets/index.js";
 import gamesRouter from './routes/games.js'
 import authRouter from './routes/auth.js';
+import { log } from 'node:console';
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
@@ -24,13 +26,28 @@ const app = express();
 const server = createServer(app);
 createSocket(server);
 
+// Cross Origin Resource Sharing
 app.use(cors(
     {
-        origin: (origin, callback) => {
-            allowedOrigins.includes(origin) ? callback(null, true) : callback(new Error('Not allowed by CORS'))
-        },
+        origin: allowedOrigins,
+        // origin: (origin, callback) => {
+        //     console.log('ORIGIN: ', origin)
+        //     console.log(allowedOrigins.indexOf('http://127.0.0.1:3001'));
+        //     allowedOrigins.indexOf(origin) !== -1 ? callback(null, true) : callback(new Error('Not allowed by CORS'))
+        // },
+        optionsSuccessStatus: 200
     }
 ))
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,14 +1,36 @@
 import express from 'express';
 import {
-    handleNewUser
+    handleNewUser,
+    handleLogin,
+    handleRefreshToken,
+    handleLogout
 } from '../controllers/authControllers/index.js'
 
 const authRouter = express.Router();
 
-authRouter.get('/register', async (req, res) => {
+authRouter.post('/register', async (req, res) => {
     const result = await handleNewUser(req.body.user, req.body.pwd);
     res.status(result.status).json(result.json)
 })
 
+authRouter.post('/login', async (req, res) => {
+    const result = await handleLogin(req.body.user, req.body.pwd);
+    console.log(result)
+    res.cookie(result.cookie.jwt, result.cookie.refreshToken, result.cookie.options)
+    res.status(result.status).json(result.json)
+})
+
+authRouter.get('/refresh', async (req, res) => {
+    console.log(req.cookies)
+    const result = await handleRefreshToken(req.cookies)
+    console.log(result)
+    res.status(result.status).json(result.json)
+})
+
+authRouter.get('/logout', async (req, res) => {
+    const result = await handleLogout(req.cookies)
+    res.clearCookie(result.cookie.jwt, result.cookie.options)
+    res.status(result.status).json(result.json)
+})
 
 export default authRouter;
