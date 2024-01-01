29ce90e42023d138e7448f7c1cb933e1a0769953
smzalam <alam.syedmz@gmail.com>
2023-12-31 19:13:49 -0500

    added middleware to verify token

---
+++ server/src/middleware/verifyJWT.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if (!authHeader) return res.sendStatus(401);
+
+    console.log(authHeader);
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            req.user = decoded.username;
+            next()
+        }
+    )
+}
+
+export default verifyJWT;
