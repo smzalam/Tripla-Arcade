e2ef257dacfad461282cb05e480ac7b129fcddab
smzalam <alam.syedmz@gmail.com>
2024-01-07 05:48:47 -0500

    modified text sizes for different screen sizes

--- client/src/components/Guide/Cards/GuideCards.jsx
+++ client/src/components/Guide/Cards/GuideCards.jsx
@@ -3,12 +3,12 @@ const GuideCards = ({game, category, type}) => {
     return (
         <div className='shrink-0 place-self-center group w-[30%] h-[90%] rounded-[20px] relative overflow-hidden'>
             <div className='absolute inset-0 grid place-content-center rounded-[10px] bg-secondary'>
-                <h1 className='text-[2rem] font-bold bg-gradient-to-r from-primary to-background bg-clip-text text-transparent'>
+                <h1 className='lg:text-[1.5rem] xl:text-[2rem] font-bold bg-gradient-to-r from-primary to-background bg-clip-text text-transparent'>
                     {game}
                 </h1>
             </div>
             <div className='group-hover:right-0 transition-all flex flex-col justify-center place-items-center w-full h-full top-0 -right-[100%] absolute bg-background opacity-95 backdrop-blur-sm rounded-[15px] text-white p-[30px]'>
-                <h1 className='uppercase text-[2em] font-bold mb-5'>{game}</h1>
+                <h1 className='uppercase lg:text-[1.5em] xl:text-[2em] font-bold mb-5'>{game}</h1>
                 <section className='m-2 bg-yellow-500 w-max py-1 px-4 rounded-lg capitalize text-[14px] font-semibold text-primary'>{category}</section>
                 <section className='m-2 bg-purple-500 w-max py-1 px-4 rounded-lg font-semibold text-[14px] text-text'>{type}</section>
  
